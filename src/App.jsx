30c2793a1d40bb3b1d34fb3c43d017ba7e0dabe2
luborn-dev <luborn.dev@gmail.com>
2024-01-07 03:15:24 -0300

    build fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import Home from "./Components/Home";
+import Header from "./Components/Header/index";
+import Footer from "./Components/Footer/index";
+import Home from "./Components/Home/index";
 import Login from "./Components/Login/Login";
 import { UserStorage } from "./UserContext";
 import User from "./Components/User/User";
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-const base = process.env.NODE_ENV === "production" ? "/dogs-web-app/" : "/";
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: base,
   plugins: [react(), svgr()],
 });
