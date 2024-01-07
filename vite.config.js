bd0f40381f23b55cccc260672c394523e841850c
luborn-dev <luborn.dev@gmail.com>
2024-01-07 02:58:06 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
+const base = process.env.NODE_ENV === "production" ? "/dogs-web-app/" : "/";
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: base,
   plugins: [react(), svgr()],
 });
